565b7a82f169f3afa4f2837d8d48631fa51d0652
Felix <30987125+flxcn@users.noreply.github.com>
2020-12-15 10:52:40 -0600

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,10 +2,10 @@
 // This file provides the information for accessing the database.and connecting to
 // MySQL. It also sets the language coding to utf-8
 // First we define the constants:
-DEFINE ('DB_USER', 'dbu568898');
-DEFINE ('DB_PASSWORD', 'VitoVito1!');
-DEFINE ('DB_HOST', 'db5000316712.hosting-data.io');
-DEFINE ('DB_NAME', 'dbs309050');
+DEFINE ('DB_USER', '****');
+DEFINE ('DB_PASSWORD', '****');
+DEFINE ('DB_HOST', '****');
+DEFINE ('DB_NAME', '****');
 // Next we assign the database connection to a variable that we will call $dbcon:
 $dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('Could not connect to MySQL: ' . mysqli_connect_error () );
