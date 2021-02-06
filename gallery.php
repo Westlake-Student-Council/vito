0a3f1f5f049c4f44c7365e894c025caf3a9abc5d
andrew-guerra <andrewjguerra10@gmail.com>
2021-02-06 13:58:29 -0600

    Updated gallery captions

--- gallery.php
+++ gallery.php
@@ -1,47 +1,55 @@
 <!doctype html>
 <html>
-<head>
-    <title>Vito - Gallery</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <?php include('header.php')?>
-    <div class="content">
-    <!--
-    <p style="text-align:center;">Have a great picture of Vito?</p>
-    <a class="button" href="submitapicture.php">Send it in!</a>
-    -->
-    <table class="gallery" width="100%">
-        <tr>
-            <td>
-                <img src="images/IMG_1931.JPG">
-            </td>
-            <td>
-                <img src="images/IMG_2358.JPG">
-            </td>
-            <td>
-                <img src="images/IMG_2636_test.jpg">
-            </td>
-            <td>
-                <img src="images/IMG_1931.JPG">
-            </td>
-        </tr>
-        <tr>
-            <td>
-            	caption/date
-            </td>
-            <td>
-                caption/date
-            </td>
-            <td>
-                caption/date
-            </td>
-            <td>
-                caption/date
-            </td>
-        </tr>
-    </table>
-    
-    </div>
-</body>
+    <head>
+        <title>Vito - Gallery</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+
+    <body>
+        <?php include('header.php')?>
+
+        <div class="content">
+        <!--
+        <p style="text-align:center;">Have a great picture of Vito?</p>
+        <a class="button" href="submitapicture.php">Send it in!</a>
+        -->
+            <table class="gallery" width="100%" border="1">
+                <tr>
+                    <td>
+                        <img src="images/IMG_1931.JPG">
+                    </td>
+                    <td>
+                        <img src="images/IMG_2358.JPG">
+                    </td>
+                    <td>
+                        <img src="images/IMG_2636_test.jpg">
+                    </td>
+                    <td>
+                        <img src="images/IMG_1931.JPG">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    	Vito laying on the track (Date Unknown)
+                    </td>
+                    <td>
+                        Vito with a bow tie (Date Unknown)
+                    </td>
+                    <td>
+                        Superhero Vito
+                    </td>
+                    <td>
+                        Vito laying on the track (Date Unknown)
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
 </html>
