2468331ca9e5550d034393fa993e1bc3f2852b27
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-01 14:41:50 -0600

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="http://vito.westlakestuco.com">Vito</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="credits.php">Credits</a>
+            </li>
+            <li class="nav-item">
+                <a class="btn btn-success" href="ask.php">Ask a Question!</a>
+                </li>
+            </ul> 
+        </div>
+    </nav>
+</header>
