e76aba186b0bde3b7bb9e3313fd1cb13baad1324
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-08 15:56:18 -0600

    Update credits.php

--- credits.php
+++ credits.php
@@ -1,4 +1,58 @@
-<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+
+    <link rel="icon" type="image/png" href="assets/images/dog.png">
+
+    <title>Vito - Credits</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/libraries/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/libraries/carousel.css" rel="stylesheet">
+
+    <style type="text/css">
+        /* .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        } */
+
+        main {
+            margin-top: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php $page_name = "credits"; include "header.php"; ?>
+
+    <main role="main">
+        
+        <!-- /.container -->
+
+        <!-- FOOTER -->
+        <footer class="container">
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <!-- <p>© 2021 Felix Chen</p> -->
+        </footer>
+    </main>
+
+    <!-- Bootstrap Core JavaScript -->
+    <!-- ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/libraries/jquery-3.2.1.slim.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/libraries/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/libraries/popper.min.js"></script>
+    <script src="assets/libraries/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+<!-- <!doctype html>
 <html>
 	<head>
 	    <title>Vito - Credits</title>
@@ -15,4 +69,4 @@
 		        <br>Alex graduates Westlake High School in 2020.</p>
 	    </div>
 	</body>
-</html>
+</html> -->
