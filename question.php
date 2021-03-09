ec3e2795d14b432e2cbee06515e496060980e918
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-08 22:43:36 -0600

    Update question.php

--- question.php
+++ question.php
@@ -100,10 +100,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>  
 
         <!-- FOOTER -->
-        <footer class="container">
-            <p class="float-right"><a href="#">Back to top</a></p>
-            <p>© 2021 Felix Chen</p>
-        </footer>
+        <?php include "footer.php"; ?>
     </main>
 
     <!-- Bootstrap Core JavaScript -->
